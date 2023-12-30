a838066c2b5d48ed790fa10e532edeb229a4aed0
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-12-30 22:01:33 +0200

    instal libs

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      {/* <Provider store={store}> */}
+      <App />
+      {/* </Provider> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
