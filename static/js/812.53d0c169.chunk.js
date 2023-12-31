0ff5e25b67ebe783ad491336010f38fe644b7a69
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-12-31 14:41:49 +0000

    Deploying to gh-pages from @ IrynaSlavinska/goit-react-hw-08-phonebook@b2422ea3564c2cac6565dee40b13d6e83beda44b 🚀

---
+++ static/js/812.53d0c169.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[812],{812:function(n,e,o){o.r(e),o.d(e,{default:function(){return s}});var t,r,i=o(168),c=o(686),h=c.Z.div(t||(t=(0,i.Z)([""]))),u=c.Z.h1(r||(r=(0,i.Z)(["\n  font-size: 60px;\n"]))),f=o(184),s=function(){return(0,f.jsx)(h,{children:(0,f.jsx)(u,{children:"Hello my friend"})})}}}]);
+//# sourceMappingURL=812.53d0c169.chunk.js.map
