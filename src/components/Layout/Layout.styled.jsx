b2422ea3564c2cac6565dee40b13d6e83beda44b
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-12-31 16:41:05 +0200

    update forms

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -34,5 +34,16 @@ export const StyledLink = styled(NavLink)`
 `;
 
 export const Main = styled.main`
-  padding: 32px;
+  background-image: url(https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  margin: 0 auto;
+  max-width: 1400px;
+  height: 85vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -1,19 +1,6 @@
 import styled from '@emotion/styled';
 
-export const HomeContainer = styled.div`
-  background-image: url(https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
-
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  margin: 0 auto;
-  max-width: 1400px;
-  height: 80vh;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+export const HomeContainer = styled.div``;
 
 export const Title = styled.h1`
   font-size: 60px;
